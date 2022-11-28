8fc2d6f39237320f3b3496dbd4d84fdd7c91d7b7
sqGoliaf <sonic.iwanow@yandex.ru>
2022-11-28 08:23:56 +0500

    Пока не понимаю redux, как с ним работать

---
+++ first-app/src/redux/toolkit/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from "redux";
+import toolkitSlice from "./toolkitSlice";
+import {configureStore} from "@reduxjs/toolkit";
+
+
+const rootReducer = combineReducers({
+    toolkit: toolkitSlice
+});
+
+export const store = configureStore({
+    reducer: rootReducer
+})
